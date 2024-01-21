986fa64d86fd7db0523f522ba1b37b36a60a16bb
heyscottn <heyscottn@gmail.com>
2024-01-21 10:11:57 -0600

    edits

--- apply.py
+++ apply.py
@@ -1,60 +1,77 @@
 print("\n---- part 1, pizza ----")
 # think of at least 3 kinds of you favorite pizza
-# store these pizza names in a list
-# print the list
-# use a for loop to print the name of each pizza
-# modify your loop to print a sentence using the name of the pizza instead of printing just the name of the pizza
-# add a line after the loop that states how much you like pizza
+# 1) store these pizza toppings in a list
+# 2) print the list
+# 3a) use a for loop to print the name of each topping
+# 3b) modify your loop to print a sentence using the name of the topping instead of printing just the name of the topping
+# for example: "I like ham"
+# 4) add a line after the loop that states how much you like pizza
+print("2) the list:")
+print("3) using a loop:")
 
 
 
 print("\n\n---- part 2, twenty numbers, no list defined ----")
-# (no list) use a for loop to print the numbers from 1 to 20, one on each line
-# (no list) use a for loop to print the nubers from 1 to 20 all on one line (hint: end=somethingsomething)
+# 1) (no list) use a for loop to print the numbers from 1 to 20, on 20 lines
+# 2) (no list) use a for loop to print the nubers from 1 to 20 all on one line (hint: end=somethingsomething)
+print("1) one to twenty:")
+print("2) one to twenty again:")
 
 
 print("\n\n---- part 3, twenty numbers, list defined ----")
-# make a list of numbers from 1 to 20 using a for loop
-# print the list
-# make a list of numbers from 1 to 20 using list comprehension
-# print the list
-
+# 1) make a list of numbers from 1 to 20 using a for loop
+# this will require the .append() method
+# 2) print the list
+# 3) make a list of numbers from 1 to 20 using list comprehension
+# 4) print the list
+print("2) one to twenty using 'for':")
+print("4) one to twenty using comprehension:")
 
 
 print("\n\n---- part 4, ten thousand numbers ----")
-# make a list of numbers from one to ten thousand
-# print the list
-# use a for loop to print each item
+# 1) make a list of numbers from one to ten thousand
+# 2) print the list
+# 3) use a for loop to print each item
 # (then comment this out)
 
 
 print("\n\n---- part 5, ten million numbers ----")
-# make a list of numbers from one to one million
-# print the length of the list
-# print the first and last items
-# verify this again by using the min and max functions to make sure that it begins and ends correctly
-# use the sum function to see how fast python can add these numbers
+# 1) make a list of numbers from one to one million
+# if necessary, sort your list
+# 2) print the length of the list
+# 3) print the first and last items
+# 4) verify this again by using the min and max functions to make sure that it begins and ends correctly
+# 5) use the sum function to see how fast python can add these numbers
+print("2) the length:")
+print("3) first and last:")
+print("4) using max and min:")
+print("5) the total:")
 
 
 print("\n\n---- part 6, odds ----")
-# make a list of odd numbers from 1 to 200
-# print the list
-# use a for loop to print each item
-
+# 1) make a list of odd numbers from 1 to 200
+# 2) print the list
+# 3) use a for loop to print each item
+print("2) the list of odds:")
+print("3) using a loop...")
 
 
 print("\n\n---- part 7, cubes ----")
 # a number raised to the 3rd power is called a cube
-# use a for loop to make a list of the first 10 cubes
-# print the list
-# use a for loop to print each item
+# 1) use a for loop to make a list of the first 10 cubes
+# 2) print the list
+# 3) use a for loop to print each item
+print("2) the list of cubes:")
+print("3) using a loop...")
+
 
 
 print("\n\n---- part 8, cubes with comprehension ----")
-# use list comprehension to make a list of the first 10 cubes
-# print the list
-# use a for loop to print each item
-
+# 1) use list comprehension to make a list of the first 10 cubes
+# 2) print the list
+# 3) use a for loop to print each item
+print("2) the list of cubes using comprehension:")
+print("3) using a loop...")
 
 
 
@@ -62,4 +79,5 @@ print("\n\n---- part 9, cold-puter science ----")
 # get input from the keyboard, all on one line separated by spaces
 # convert to a list of integers
 # determine how many are less than zero
+
 
