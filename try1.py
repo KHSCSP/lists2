49d25a26a37be284f8e2c97a52500a128f43bfae
heyscottn <heyscottn@gmail.com>
2024-01-21 10:20:59 -0600

    edits

--- try1.py
+++ try1.py
@@ -3,31 +3,41 @@ print(" --- even numbers ---")
 # even numbers
 # noob - explicitly defined
 evens = [2,4,6,8,10,12]
+print("the list:")
 
 # better - loop
-
+print("creating evens with a loop:")
 
 # pro - list comprehension
+print("creating evens with list comprehension:")
 
 
 
 print("\n\n --- multiples of 3 ---")
 # threes (noob & pro)
+print("threes:")
+print("creating threes with list comprehension:")
 
 
 
 print("\n\n --- squares ---")
 # squares (noob & pro)
+print("squares:")
+print("creating squares with list comprehension:")
+
 
 
 print("\n\n-------------------------------------")
 print(" --- other functions ---")
 # other functions: min, max, sum
+print("finding smallest using min:")
+print("finding largest using max:")
+print("finding total using sum:")
 
 # noob (or when your teacher makes you)
+print("finding total using a loop:")
 
 
-# pro - built in
 
 
 print("\n\n-------------------------------------")
@@ -36,8 +46,13 @@ print(" --- longest string ---")
 foods = ["banana", "grape", "apple", "lettuce", "potato", "pea", "corn"]
 
 
+print("i found the longest word:")
+
 
 
 
 print("\n\n-------------------------------------")
 print(" --- converting input ---")
+
+
+print("converted to a list:")
